8b42203f4dbfb03752a892925e0953236f2e0224
jo5ean <josean.urbani@gmail.com>
2025-01-08 17:36:27 -0300

    Home, navigation, footer & dinamic pages

---
+++ src/types/adventure.ts
@@ -0,0 +1,13 @@
+export interface Adventure {
+    id: string;
+    title: string;
+    location: string;
+    image: string;
+    gallery: string[];
+    description: string;
+    category: string;
+    duration: string;
+    difficulty: string;
+    date: string[];
+    price: number;
+}

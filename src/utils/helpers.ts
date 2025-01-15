faf0147eea656bb74a4f65a79d40cc311b9f0c84
jo5ean <josean.urbani@gmail.com>
2025-01-15 01:30:24 -0300

    fixes, fixes everywhere

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,9 @@
 export const formatName = (name: string) => {
     return encodeURIComponent(
         name
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
             .replaceAll(" ", "-")
             .replaceAll("/", "-")
             .replaceAll("?", "-")
@@ -17,6 +20,18 @@ export const formatName = (name: string) => {
             .replaceAll("'", "-")
             .replaceAll('"', "-")
             .replaceAll(",", "-")
-            .replaceAll(".", "-"),
+            .replaceAll(".", "-")
+            .replaceAll("á", "a")
+            .replaceAll("é", "e")
+            .replaceAll("í", "i")
+            .replaceAll("ó", "o")
+            .replaceAll("ú", "u")
+            .replaceAll("ñ", "n")
+            .replaceAll("Á", "a")
+            .replaceAll("É", "e")
+            .replaceAll("Í", "i")
+            .replaceAll("Ó", "o")
+            .replaceAll("Ú", "u")
+            .replaceAll("Ñ", "n")
     );
 };
